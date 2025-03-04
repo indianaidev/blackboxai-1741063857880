1748ef4d829c1da9fd4279c6e652ea173c61c21f
indianaidev <amanmaurya2314@gmail.com>
2025-03-04 15:16:42 +0530

    Add project/drizzle.config.ts 1741081601993

---
+++ project/drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  schema: './src/lib/db/schema.ts',
+  out: './drizzle',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.VITE_NEON_DATABASE_URL!,
+  },
+} satisfies Config;
